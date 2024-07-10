9841be8f91f7e1568008c0c0e542a796d996fb10
Rachel Kim <rachelgekim@gmail.com>
2024-07-10 11:41:18 -0400

    Add ability to aim

---
+++ Assets/Aim.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [Header("Aim Controls")]
+    [SerializeField] private string aimUpKey;
+    [SerializeField] private string aimDownKey;
+
+    [Header("Bounds")]
+    [SerializeField] private float upperBound = 35f;
+    [SerializeField] private float lowerBound = -25f;
+
+    [Header("Attributes")]
+    [SerializeField] private float aimSpeed = 0.5f;
+
+    void Update()
+    {
+        float zRotation = gameObject.transform.eulerAngles.z;
+
+        if (zRotation > 180)
+        {
+            zRotation -= 360;
+        }
+
+        if (Input.GetKey(aimUpKey) && zRotation <= upperBound)
+        {
+            zRotation += aimSpeed;
+            zRotation = Mathf.Clamp(zRotation, lowerBound, upperBound);
+            gameObject.transform.eulerAngles = new Vector3(0, 0, zRotation);
+        }
+
+        if (Input.GetKey(aimDownKey) && zRotation >= lowerBound)
+        {
+            zRotation -= aimSpeed;
+            zRotation = Mathf.Clamp(zRotation, lowerBound, upperBound);
+            gameObject.transform.eulerAngles = new Vector3(0, 0, zRotation);
+        }
+    }
+}
